7f465664e1e9e596edcfce78e677d3663cc88ff0
Alvin George <alvingeorge_@outlook.com>
2024-10-15 12:35:26 +0530

    scripts

---
+++ scripts.js
@@ -0,0 +1,31 @@
+//console.log('Hello MEC')
+// let x = 5;
+// console.log(x);
+// x = 10;
+// console.log(x);
+// const y = 12;
+// console.log(y);
+// // y = 13;
+// // console.log(y)
+// x = "helo"
+// console.log(x)
+
+// if(9<8){
+//     console.log(1)
+// }
+// else{
+//     console.log(0)
+// }
+let count = 0;
+let count2 = 2;
+function myFunc(param1){
+    count += count2;
+    console.log(` Hello ${param1}. Your count is: ${count}. You're welcome`)
+}
+
+function changeButton(){
+    if(document.getElementById('subbuto').innerHTML=== 'Subscribe'){
+        document.getElementById('subbuto').innerHTML=== "Subscribed"
+    }
+}
+myFunc()
